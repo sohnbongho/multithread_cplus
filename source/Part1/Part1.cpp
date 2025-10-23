ef4386e44e2ce1d0adde6d83d2c0738b0eecf09f
bhsohn <hanmuson1@gmail.com>
2025-10-23 10:30:58 +0900

    소스 이동

--- source/Part1/Part1.cpp
+++ source/Part1/Part1.cpp
@@ -1,25 +1,21 @@
-﻿// Example of C++11 universal initialization
+﻿// Example of range-for loop
 #include <iostream>
-#include <string>
 #include <vector>
 
 int main()
 {
-	int x{ 7 };                                         // Equivalent to int x = 7;
-	std::string str{ "Let us begin" };                  // Equivalent to string str("Let us begin");
+	std::vector<int> vec{ 4, 2, 3, 5, 1 };
 
-	std::cout << "x = " << x << '\n';
-	std::cout << "str = \"" << str << "\"\n";
+	std::cout << "Vector elements before: ";
+	for (auto i : vec)
+		std::cout << i << ", ";
+	std::cout << '\n';
 
-	// Narrowing conversions are not allowed
-	int y = 7.7;                                      // Legal, although compilers may warn
-	//int y{7.7};                                     // Illegal
-	std::cout << "y = " << y << '\n';
+	// We need to use a reference to modify the elements
+	for (auto& i : vec)
+		i += 2;
 
-	// Can  be used with multiple initial values
-	std::vector<int> vec{ 4, 2, 3, 5, 1 };              // std::vector variable with elements 4, 2, 3, 5, 1
-
-	std::cout << "vec = ";
-	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
-		std::cout << *it << ", ";
+	std::cout << "Vector elements after: ";
+	for (auto i : vec)
+		std::cout << i << ", ";
 }
